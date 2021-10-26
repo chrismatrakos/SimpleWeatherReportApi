31f25f271c9b873abd23f41f5e03ddaeb4c8e6f7
Matrakou Christo-PMHK38 <Christo.Matrakou@motorolasolutions.com>
2021-10-26 11:02:27 +0200

    simple weather report api

---
+++ app.js
@@ -0,0 +1,53 @@
+var mainApp = angular.module("mainApp", []);
+
+mainApp.value("defaultInput", "type to search another city...");
+
+mainApp.factory('WeatherApiService', function($http) {
+    var factory = {};
+    factory.requestWeatherByCity = function(town){
+        var URL = 'http://api.openweathermap.org/data/2.5/find?';
+  
+        var request = {
+            method: 'GET',
+            url: URL,
+            params: {
+                q: town,
+                mode: 'json',
+                units: 'metric',
+                cnt: '7',
+                appid: '1cb6ace31e50401f28b864f0b23fdc68'
+            }
+        };
+        return $http(request);
+    }
+
+    return factory;
+});
+
+mainApp.service('WeatherService', function(WeatherApiService){
+    this.getWeatherByCity = function(town){
+        return WeatherApiService.requestWeatherByCity(town);
+    }
+});
+
+mainApp.controller('WeatherController', function($scope, WeatherService, defaultInput) {
+  $scope.number = defaultInput;
+    $scope.square = function() {
+        WeatherService.getWeatherByCity($scope.searchCity).then(function(response) {
+            console.log(response.data);// has more fields!!!
+
+            //check and prompt unique city
+
+            $scope.city = response.data.name;
+            for (var obj of response.data.list) {
+                console.log(obj.main.temp)
+                $scope.city = obj.name;
+                $scope.temp = obj.main.temp;
+                $scope.hum = obj.main.humidity;
+                $scope.wind = obj.wind.speed;
+                $scope.code = obj.weather[0].description;
+            }
+            //to do: later add more fields to weather widget!!!           
+        });
+    }
+});
