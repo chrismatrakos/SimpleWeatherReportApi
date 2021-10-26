d32458da0244a6730bcdb15ea3273eb87b7acf2b
Matrakou Christo-PMHK38 <Christo.Matrakou@motorolasolutions.com>
2021-10-26 13:25:16 +0200

    add lon lat

--- WeatherReportApi/app.js
+++ WeatherReportApi/app.js
@@ -4,6 +4,22 @@ mainApp.value("searchCity", "type to search another city...");
 
 mainApp.factory('WeatherApiService', function($http) {
     var factory = {};
+    factory.requestWeatherByLocation = function(a,b) {
+        var URL = "http://api.openweathermap.org/geo/1.0/reverse?lat="+a+"&lon="+b;
+
+        var request = {
+            method: 'GET',
+            url: URL,
+            params: {
+                mode: 'json',
+                units: 'metric',
+                cnt: '7',
+                appid: '1cb6ace31e50401f28b864f0b23fdc68'
+            }
+        };
+        return $http(request);        
+    }
+
     factory.requestWeatherByCity = function(town){
         var URL = 'http://api.openweathermap.org/data/2.5/find?';
   
@@ -25,6 +41,9 @@ mainApp.factory('WeatherApiService', function($http) {
 });
 
 mainApp.service('WeatherService', function(WeatherApiService){
+    this.getWeatherByLocation = function(a, b) {
+        return WeatherApiService.requestWeatherByLocation(a,b);        
+    }
     this.getWeatherByCity = function(town){
         return WeatherApiService.requestWeatherByCity(town);
     }
@@ -37,23 +56,33 @@ mainApp.controller('WeatherController', function($scope, WeatherService, searchC
             console.log(response.data);// has more fields!!!
 
             //check and prompt unique city
-
-            $scope.city = response.data.name;
-            $scope.names = response.data.list
-            $scope.country = "GB"
-
-            for (var obj of response.data.list) {
-                if ($scope.country == obj.sys.country){
+            if (response.data.list.length == 1) {
+                var obj = response.data.list[0];
+                console.log(obj)
+                $scope.city = obj.name;
+                $scope.country = obj.sys.country;
+                $scope.temp = obj.main.temp;
+                $scope.hum = obj.main.humidity;
+                $scope.wind = obj.wind.speed;
+                $scope.code = obj.weather[0].description;
+            }
+            else {
+                $scope.city = response.data.name;
+                $scope.names = response.data.list    
+                for (var obj of response.data.list) {
+                    WeatherService.getWeatherByLocation(obj.coord.lat, obj.coord.lon).then(function(response) {
+                        console.log(response.data);
+                    });
                     console.log(obj.main.temp)
                     $scope.city = obj.name;
+                    $scope.country = obj.sys.country;
                     $scope.temp = obj.main.temp;
                     $scope.hum = obj.main.humidity;
                     $scope.wind = obj.wind.speed;
-                    $scope.code = obj.weather[0].description;
+                    $scope.code = obj.weather[0].description;                
                 }
-                
             }
-            //to do: later add more fields to weather widget!!!           
         });
     }
 });
+
