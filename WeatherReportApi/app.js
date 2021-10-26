f4f94ec098d04654ecbb6d24dfced42e2f9b192b
Matrakou Christo-PMHK38 <Christo.Matrakou@motorolasolutions.com>
2021-10-26 11:49:22 +0200

    start filtering

--- WeatherReportApi/app.js
+++ WeatherReportApi/app.js
@@ -1,6 +1,6 @@
 var mainApp = angular.module("mainApp", []);
 
-mainApp.value("defaultInput", "type to search another city...");
+mainApp.value("searchCity", "type to search another city...");
 
 mainApp.factory('WeatherApiService', function($http) {
     var factory = {};
@@ -30,8 +30,8 @@ mainApp.service('WeatherService', function(WeatherApiService){
     }
 });
 
-mainApp.controller('WeatherController', function($scope, WeatherService, defaultInput) {
-  $scope.number = defaultInput;
+mainApp.controller('WeatherController', function($scope, WeatherService, searchCity) {
+  $scope.searchCity = searchCity;
     $scope.square = function() {
         WeatherService.getWeatherByCity($scope.searchCity).then(function(response) {
             console.log(response.data);// has more fields!!!
@@ -39,13 +39,19 @@ mainApp.controller('WeatherController', function($scope, WeatherService, default
             //check and prompt unique city
 
             $scope.city = response.data.name;
+            $scope.names = response.data.list
+            $scope.country = "GB"
+
             for (var obj of response.data.list) {
-                console.log(obj.main.temp)
-                $scope.city = obj.name;
-                $scope.temp = obj.main.temp;
-                $scope.hum = obj.main.humidity;
-                $scope.wind = obj.wind.speed;
-                $scope.code = obj.weather[0].description;
+                if ($scope.country == obj.sys.country){
+                    console.log(obj.main.temp)
+                    $scope.city = obj.name;
+                    $scope.temp = obj.main.temp;
+                    $scope.hum = obj.main.humidity;
+                    $scope.wind = obj.wind.speed;
+                    $scope.code = obj.weather[0].description;
+                }
+                
             }
             //to do: later add more fields to weather widget!!!           
         });
