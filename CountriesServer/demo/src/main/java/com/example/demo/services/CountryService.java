14d8eed2d2e64a6f7478bdbb735a97e206735163
Christo.Matrakou <chrismatrakou@gmail.com>
2021-10-28 04:30:03 +0200

    fix return type

--- CountriesServer/demo/src/main/java/com/example/demo/services/CountryService.java
+++ CountriesServer/demo/src/main/java/com/example/demo/services/CountryService.java
@@ -1,8 +1,11 @@
 package com.example.demo.services;
+
 import com.example.demo.models.Country;
-import java.util.List;
-import java.util.ArrayList;
 import com.example.demo.repositories.CountryRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -11,42 +14,45 @@ import reactor.core.publisher.Mono;
 
 @Service
 public class CountryService implements ICountryService {
-    // private final WebClient webClient;
 
-    // @Autowired
-    // public CountryService(WebClient.Builder webClientBuilder) {
-		  //  this.webClient = webClientBuilder.baseUrl("https://restcountries.com/v3.1").build();
-	   // }
-    
-    @Override
-    public List<Country> getAll() {
-     WebClient client = WebClient.create();
-     WebClient.ResponseSpec responseSpec = client.get().uri("https://restcountries.com/v3.1/all").retrieve();
-     String responseBody = responseSpec.bodyToMono(String.class).block();
-     System.out.println(responseBody);
-      // return this.webClient.get().uri("/all")
-						// .retrieve();
-      // .bodyToMono(Country.class);
-      // WebClient webClient = WebClient.create();
-      Country c = new Country("C", "A");
-      List<Country> list = new ArrayList<Country>();
-      list.add(c);
-      return list;
-    }
-    
-    @Override
-    public String getInit(String text){
-     	return String.format("Hello %s!", text);
-    }
+  @Override
+  public List<Country> getAll() {
+    WebClient client = WebClient.create();
+    WebClient.ResponseSpec responseSpec = client
+      .get()
+      .uri("https://restcountries.com/v3.1/all")
+      .retrieve();
+    String responseBody = responseSpec.bodyToMono(String.class).block();
+    System.out.println(responseBody);
+    Country c = new Country("C", "A");
+    List<Country> list = new ArrayList<Country>();
+    list.add(c);
+    return list;
+  }
 
-    @Override
-    public String findByName(String name){
-     WebClient client = WebClient.create();
-     WebClient.ResponseSpec responseSpec = client.get().uri("https://restcountries.com/v3.1/name/" +name ).retrieve();
-     String responseBody = responseSpec.bodyToMono(String.class).block();
-     
-     System.out.println(responseBody);
-     
-     return String.format("Hello %s!", name);
+  @Override
+  public String getInit(String text) {
+    return String.format("Hello %s!", text);
+  }
+
+  @Override
+  public String findByName(String name) {
+    WebClient client = WebClient.create();
+    WebClient.ResponseSpec responseSpec = client
+      .get()
+      .uri("https://restcountries.com/v3.1/name/" + name)
+      .retrieve();
+    String responseBody = responseSpec.bodyToMono(String.class).block();
+    ObjectMapper mapper = new ObjectMapper();
+    List<Object> slist = new ArrayList<>();
+
+    try
+    {
+     slist = Arrays.asList(mapper.readValue(responseBody, Object[].class));
     }
+    catch(Exception e){System.out.println("FAILED");}
+    System.out.println(slist);
+
+    return String.format("Hello %s!", responseBody);
+  }
 }
