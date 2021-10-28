366d981ed86cf4711d0a79b1a16150542bab6b1a
Christo.Matrakou <chrismatrakou@gmail.com>
2021-10-28 06:15:29 +0200

    add

--- CountriesServer/demo/src/main/java/com/example/demo/controllers/CountryController.java
+++ CountriesServer/demo/src/main/java/com/example/demo/controllers/CountryController.java
@@ -1,9 +1,10 @@
 package com.example.demo.controllers;
+
 import com.example.demo.models.Country;
+import com.example.demo.repositories.CountryRepository;
+import com.example.demo.services.ICountryService;
 import java.util.List;
 import java.util.function.*;
-import com.example.demo.services.ICountryService;
-import com.example.demo.repositories.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,32 +12,35 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import org.springframework.data.repository.CrudRepository;
+
 
 @RestController
-public class CountryController {  
-  
+public class CountryController {
+
   @Autowired
   private ICountryService countryService;
 
   CountryController() {}
-  
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-			return countryService.getInit(name);
-	}
-  
+
+  @GetMapping("/hello")
+  public String hello(
+    @RequestParam(value = "name", defaultValue = "World") String name) {
+    return countryService.getInit(name);
+  }
+
   @GetMapping("/countries")
   List<Country> all() {
     return countryService.getAll();
   }
-  
+
   @GetMapping("/countries/{name}")
   String getCountry(@PathVariable String name) {
     return countryService.findByName(name);
-      // .orElseThrow(() -> new CountryNotFoundException(name));
+    // .orElseThrow(() -> new CountryNotFoundException(name));
   }
 }
--- CountriesServer/demo/src/main/java/com/example/demo/services/CountryService.java
+++ CountriesServer/demo/src/main/java/com/example/demo/services/CountryService.java
@@ -8,14 +8,20 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferUtils;
+import org.springframework.http.codec.ServerCodecConfigurer;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
 public class CountryService implements ICountryService {
-private final static ObjectMapper lenientMapper = new ObjectMapper();
+
+  private static final ObjectMapper lenientMapper = new ObjectMapper();
 
   @Override
   public List<Country> getAll() {
@@ -32,7 +38,7 @@ private final static ObjectMapper lenientMapper = new ObjectMapper();
 
   @Override
   public String getInit(String text) {
-    return String.format("Hello %s!", text);
+    return String.format("Welcome to server %s!", text);
   }
 
   @Override
@@ -42,23 +48,34 @@ private final static ObjectMapper lenientMapper = new ObjectMapper();
       .get()
       .uri("https://restcountries.com/v3.1/name/" + name)
       .retrieve();
-    
-    String responseBody = responseSpec.bodyToMono(String.class).block();  
-    parseResponse(responseBody);
-    return String.format("Hello %s!", responseBody);
-  }
-  
-  private void  parseResponse(String responseBody){
-    ObjectMapper mapper = new ObjectMapper();
-    List<Object> slist = new ArrayList<>();
 
+    String responseBody = responseSpec.bodyToMono(String.class).block();
+    return String.format("%s!", parseCountryByName(responseBody));
+  }
+
+  private String parseCountryByName(String responseBody) {
+    ObjectMapper mapper = new ObjectMapper();
+    List<Object> countriesList = new ArrayList<>();
     try {
-      slist = Arrays.asList(mapper.readValue(responseBody, Object[].class));
+      countriesList =
+        Arrays.asList(mapper.readValue(responseBody, Object[].class));
     } catch (Exception e) {
       System.out.println("FAILED");
     }
-    
-    System.out.println(slist.get(0));
+
+    return String.format("%s!", countriesList.get(0).toString());
     // Country c = lenientMapper.readValue(slist.get(0), Country.class);
+  }
+
+  private List<Country> parseResponse(String responseBody) {
+    ObjectMapper mapper = new ObjectMapper();
+    List<Object> countriesList = new ArrayList<>();
+    try {
+      countriesList =
+        Arrays.asList(mapper.readValue(responseBody, Object[].class));
+    } catch (Exception e) {
+      System.out.println("FAILED");
+    }
+    return new ArrayList<>();
   }
 }
