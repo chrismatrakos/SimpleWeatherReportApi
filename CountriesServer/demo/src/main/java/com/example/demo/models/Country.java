ea9ac22d038352c3b3fa93db00a1b84aa27f7443
Matrakou Christo-PMHK38 <Christo.Matrakou@motorolasolutions.com>
2021-10-28 08:45:16 +0200

    update models

--- CountriesServer/demo/src/main/java/com/example/demo/models/Country.java
+++ CountriesServer/demo/src/main/java/com/example/demo/models/Country.java
@@ -36,24 +36,86 @@ public class Country {
     this.name = name;
   }
 
+  public String getCapital() {
+    return this.capital;
+  }
+
+  public void setCapital(String capital) {
+    this.capital = capital;
+  }
+
+  public String[] getTopLevelDomain() {
+    this.topLevelDomain = topLevelDomain;
+  }
+
+  public void setTopLevelDomain(String[] topLevelDomain) {
+    this.capital = topLevelDomain;
+  }
+
+  public String[] getAltSpellings() {
+    return this.altSpellings;
+  }
+ 
+  public void setAltSpellings(String [] altSpellings) {
+    this.altSpellings = altSpellings;
+  }
+  
+  public String getAlpha2Code() {
+    return this.alpha2Code;
+  }
+ 
+  public void setAlpha2Code(String alpha2Code) {
+    this.alpha2Code = alpha2Code;
+  }
+
+  public String getAlpha3Code() {
+    return this.alpha3Code;
+  }
+ 
+  public void setAlpha3Code(String alpha3Code) {
+    this.alpha3Code = alpha3Code;
+  }
+
+  public String getRegion() {
+    return this.region;
+  }
+ 
+  public void setRegion(String region) {
+    this.region = region;
+  }
+
+  public int[] getCallingCodes() {
+    return this.callingCodes;
+  }
+ 
+  public void setCallingCodes(int[] callingCodes) {
+    this.callingCodes = callingCodes;
+  }
+
   @Override
   public boolean equals(Object o) {
-
     if (this == o)
       return true;
     if (!(o instanceof Country))
       return false;
     Country country = (Country) o;
-    return Objects.equals(this.name, country.name);
+    return Objects.equals(this.name, country.name) &&
+      Objects.equals(this.capital, country.capital) &&
+      Objects.equals(this.topLevelDomain, country.topLevelDomain) &&
+      Objects.equals(this.altSpellings, country.altSpellings) &&
+      Objects.equals(this.alpha2Code, country.alpha2Code) &&
+      Objects.equals(this.alpha3Code, country.alpha3Code) &&
+      Objects.equals(this.callingCodes, country.callingCodes) &&
+      Objects.equals(this.region, country.region);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(this.name);
+    return Objects.hash(this.name, this.capital,this.topLevelDomain, this.altSpellings, this.alpha2Code,this.alpha3Code, this.callingCodes, this.region);
   }
 
   @Override
   public String toString() {
-    return "Country{ " + "name=" + this.name + "}";
+    return "Country{ " + "name=" + this.name + "capital=" + this.capital + + "topLevelDomain=" + this.topLevelDomain + "altSpellings=" + this.altSpellings + "alpha2Code=" + this.alpha2Code + "alpha3Code=" + this.alpha3Code + "callingCodes=" + this.callingCodes + "region=" + this.region +"}";
   }
 }
